67ab7cad864336aefa3751392d3d27a80b813801
rohan-krishna <phonemg30993@gmail.com>
2015-07-14 17:29:19 +0530

    Offers Content

--- page-offers.php
+++ page-offers.php
@@ -17,6 +17,7 @@ Template Name: Offers Page
 		<div class="col-md-12">
 			<div class="col-md-6 col-md-offset-3 text-center">
 				<?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
 				<?php endwhile; endif; ?>
 			</div>
 		</div>
